58c9cc67ad5b36191f956ba4ccb260775cbcd999
Megan Schanz <schanzme@msu.edu>
2020-06-15 16:33:56 -0400

    switched structure of json loader config to a list of paths instead of a dict

--- sandhill/processors/file.py
+++ sandhill/processors/file.py
@@ -7,9 +7,9 @@ def load_json(data_dict):
     file_data = collections.OrderedDict()
 
     # loop over each provided path and stop when one is found
-    if 'params' in data_dict:
-        for jk, jv in data_dict['params'].items():
-            full_path = os.path.join(app.instance_path, jv)
+    if 'paths' in data_dict:
+        for path in data_dict['paths']:
+            full_path = os.path.join(app.instance_path, path)
             if os.path.exists(full_path):
                file_data = json.load(open(full_path), object_pairs_hook=collections.OrderedDict)
                break
