ab694303ebab931f3be30a1dae5ab6a4118e98bd
Nathan Collins <colli372@msu.edu>
2020-08-04 17:01:40 -0400

    Added tests; fixin' code

--- sandhill/routes/tests/test_main.py
+++ sandhill/routes/tests/test_main.py
@@ -4,3 +4,8 @@ def test_home():
     client = app.test_client()
     result = client.get('/')
     assert result.status_code == 200
+
+def test_404():
+    client = app.test_client()
+    result = client.get('/invalid/page/route')
+    assert result.status_code == 404
--- sandhill/utils/generic.py
+++ sandhill/utils/generic.py
@@ -2,17 +2,11 @@
 Generic function that can be used in any context
 """
 
-def ifnone(var, default_val):
-    '''
-    Returns var if it is not None else the default_val
-    '''
-    return var if var else default_val
-
 def ifnone(var, key, default_val):
     '''
     Returns var[key] if key in var else the default_val
     '''
-    return var[key] if key in var else default_val
+    return var[key] if isinstance(var, dict) and key in var else default_val
 
 def combine_to_list(*args):
     """
