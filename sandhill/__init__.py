9dfcb5dcc32d24288fe46fb88016dee14cde88c8
Megan Schanz <schanzme@msu.edu>
2020-08-06 14:08:45 -0400

    adds the debug sidebar, but on all environments currently

--- sandhill/__init__.py
+++ sandhill/__init__.py
@@ -1,10 +1,15 @@
 import os
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 install_dir = os.path.dirname(os.path.dirname(__file__))
 app = Flask(__name__, instance_relative_config=True)
 # Local imports requiring the Flask app
 from .utils import setup, filters
 from .routes import main
 
+app.debug = True
+toolbar = DebugToolbarExtension(app)
+
 if __name__ == "__main__":
     app.run(debug=True)
+    toolbar.init_app(app)
