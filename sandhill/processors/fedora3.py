8fab1f0150221b2ec2808a790c2cc73793cb293a
Devin Higgins <higgi135@lib.msu.edu>
2020-08-19 16:12:00 -0400

    Add coverage for fedora processor

--- sandhill/processors/fedora3.py
+++ sandhill/processors/fedora3.py
@@ -1,26 +1,29 @@
 import os
+import validators
 from urllib.parse import urlencode
 from flask import abort
 from sandhill.utils.api import api_get
 from sandhill import app
 from requests import RequestException
 
-def load_datastream(data_dict):
+def load_datastream(data_dict, fedora_url=None, api_get_function=api_get):
     fedora = None
     allowed_actions = ['view', 'download']
-    if data_dict['view_args']['action'] not in allowed_actions:
+    if not fedora_url:
+        fedora_url = os.environ.get('FEDORA_URL', None)
+    if data_dict['view_args']['action'] not in allowed_actions or not fedora_url or not validators.url(fedora_url):
         abort(400)
     try:
         params = {}
         params['download'] = 'true' if data_dict['view_args']['action'] == "download" else 'false'
-        fc_url = os.path.join(os.environ.get('FEDORA_URL'),
+        fc_url = os.path.join(fedora_url,
                         "objects/{0}:{1}/datastreams/{2}/content".format(
                         data_dict['view_args']['namespace'],
                         data_dict['view_args']['id'],
                         data_dict['view_args']['label'])
                     )
         app.logger.debug("Connecting to {0}?{1}".format(fc_url, urlencode(params)))
-        fedora = api_get(url=fc_url, params=params, stream=True)
+        fedora = api_get_function(url=fc_url, params=params, stream=True)
 
         if not fedora.ok:
             app.logger.warning("Call to Fedora Commons returned {0}".format(fedora.status_code))
@@ -28,4 +31,7 @@ def load_datastream(data_dict):
     except RequestException as exc:
         app.logger.warning("Call to Fedora Commons failed: {0}".format(exc))
         abort(503)
+    except KeyError as exc:
+        app.logger.error("Missing url component: {0}".format(exc))
+        abort(400)
     return fedora
---
+++ sandhill/processors/tests/test_fedora3.py
@@ -0,0 +1,76 @@
+from pytest import raises
+from werkzeug.exceptions import HTTPException
+from sandhill.processors import fedora3
+from requests.models import Response
+from requests.exceptions import RequestException
+
+def test_conditions():
+    data_dict = {
+        "view_args": {
+            "action": "view",
+            "namespace": "etd",
+            "id": "100",
+            "label": "TN"
+        }
+    }
+    # Test for successful response
+    response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get)
+    assert isinstance(response, Response)
+    assert response.ok
+
+    # Test for successful response with 'download' action
+    data_dict["view_args"]["action"] = "download"
+    response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get)
+    assert isinstance(response, Response)
+    assert response.ok
+
+    # Test for invalid 'action' setting.
+    data_dict["view_args"]["action"] = "Invalid"
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get)
+    assert http_error.type.code == 400
+
+    # Test for invalid fedora_url
+    data_dict["view_args"]["action"] = "download"
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url="invalid url", api_get_function=_test_api_get)
+    assert http_error.type.code == 400
+
+    # Test for invalid fedora_url
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url=None, api_get_function=_test_api_get)
+    assert http_error.type.code == 400
+
+    # Test for failed call to fedora
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get_unavailable)
+    assert http_error.type.code == 503
+
+    # Test for non-503 fedora call exception
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get_fail)
+    assert http_error.type.code == 500
+
+    # Test for missing url parameter 'namespace'
+    del data_dict["view_args"]["namespace"]
+    with raises(HTTPException) as http_error:
+        response = fedora3.load_datastream(data_dict, fedora_url="https://test.example.edu", api_get_function=_test_api_get)
+    assert http_error.type.code == 400
+
+
+
+def _test_api_get(url=None, params=None, stream=True):
+    """Test function to simulate fedora api call."""
+    response = Response()
+    response.status_code = 200
+    return response
+
+def _test_api_get_fail(url=None, params=None, stream=True):
+    """Test function to always fail."""
+    response = Response()
+    response.status_code = 500
+    return response
+
+def _test_api_get_unavailable(url=None, params=None, stream=True):
+    """Test function to always fail."""
+    raise RequestException()
