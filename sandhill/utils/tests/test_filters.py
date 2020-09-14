eada99b9abf4281fba209c73cc36c959fbbf91b7
Megan Schanz <schanzme@msu.edu>
2020-09-14 09:29:31 -0400

    just want to save what we tried. does not work

--- sandhill/utils/tests/test_filters.py
+++ sandhill/utils/tests/test_filters.py
@@ -1,4 +1,5 @@
 from sandhill.utils import filters
+from sandhill import app
 
 def test_islist():
     assert filters.is_list([]) == True
@@ -6,3 +7,20 @@ def test_islist():
     assert filters.is_list({'a':1}) == False
     assert filters.is_list("string") == False
     assert filters.is_list(None) == False
+
+def test_render():
+
+    data_dict = {
+        "environment": {
+            "var": "val"
+        }
+    }
+
+    # Test rendering a value from the context
+    with app.test_request_context('/etd/1000'):
+        #ctx = app.app_context()
+        print(app.__dict__)
+        print(app.template_context_processors.__dict__)
+        print(type(app.jinja_env.render(data_dict)))
+        print(app.jinja_env.render(data_dict))
+        assert filters.render("just no", "{{ var }}") == "val"
