0be37abfe9ad53e2351bd5bbbcb0281d30264538
Devin Higgins <higgi135@lib.msu.edu>
2021-02-09 14:59:47 -0500

    Fixed issues with debug toolbar preventing tests from running -- all tests now working

--- sandhill/__init__.py
+++ sandhill/__init__.py
@@ -4,11 +4,7 @@ Creates the Flask application and is the entry point for the application
 import os
 from flask import Flask
 install_dir = os.path.dirname(os.path.dirname(__file__))
-app = Flask(__name__,
-            instance_relative_config=True,
-            instance_path=os.environ["TEST_INSTANCE_DIR"]
-            if "TEST_INSTANCE_DIR" in os.environ
-            else None)
+app = Flask(__name__, instance_relative_config=True)
 
 # Local imports requiring the Flask app
 from sandhill.utils import setup, filters # pylint: disable=wrong-import-position
