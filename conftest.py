b60cbb88793aa1c8d9019c08b3c3ac7fa5ad128d
Devin Higgins <higgi135@lib.msu.edu>
2021-02-09 16:36:15 -0500

    Restore changes to os environ and Flask instantiation

--- conftest.py
+++ conftest.py
@@ -6,4 +6,5 @@ from flask import Flask
 import sys
 cur_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, cur_path)
-os.environ["TEST_INSTANCE_DIR"] = os.path.join(cur_path, "sandhill/test_instance/")
+if "TEST_INSTANCE_DIR" not in os.environ or not os.environ["TEST_INSTANCE_DIR"]:
+    os.environ["TEST_INSTANCE_DIR"] = os.path.join(cur_path, "sandhill/test_instance/")
--- sandhill/__init__.py
+++ sandhill/__init__.py
@@ -4,8 +4,11 @@ Creates the Flask application and is the entry point for the application
 import os
 from flask import Flask
 install_dir = os.path.dirname(os.path.dirname(__file__))
-app = Flask(__name__, instance_relative_config=True)
-
+app = Flask(__name__,
+            instance_relative_config=True,
+            instance_path=os.environ["TEST_INSTANCE_DIR"]
+            if "TEST_INSTANCE_DIR" in os.environ
+            else None)
 # Local imports requiring the Flask app
 from sandhill.utils import setup, filters # pylint: disable=wrong-import-position
 from sandhill.routes import main, static, error # pylint: disable=wrong-import-position
