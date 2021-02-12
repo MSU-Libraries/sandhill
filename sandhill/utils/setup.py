ef17ccd7010c7343ccfc46238d2c48dbe80f23dc
Megan Schanz <schanzme@msu.edu>
2021-02-12 12:44:59 -0500

    logging works, just needs: refactoring, input validation, cicd

--- sandhill/utils/setup.py
+++ sandhill/utils/setup.py
@@ -4,11 +4,62 @@ import logging
 import sys
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask.logging import create_logger
 from sandhill import app
 from sandhill.commands.compile_scss import run_compile
 from jinja2 import ChoiceLoader, FileSystemLoader
 from sassutils.wsgi import SassMiddleware
+
+def configure_logging():
+    '''
+    Configure application logging. Includes: default logger, file logger, 
+    and email based on the application configuration file.
+    '''
+    # TODO -- CI/CD options for the config file
+    #       1) use the default config for test, stage, and prod
+    #       2) make separate configs and have the deploy job copy appropriately
+    #           (ex: sandhill.test.cfg -> sandhill.cfg, sandhill.prod.cfg -> sandhill.cfg, ...)
+    #       3) make a sandhill.cicd.cfg which has placeholders that we do a sed command to set
+    #       4) have the deploy job echo configs into a new file
+    # TODO -- do we want the format string to be in the config file too?
+    #           https://docs.python.org/3/library/logging.html#logrecord-attributes
+    # TODO -- how much validation do we want to do? valid emails? valid log levels? file path?
+
+    # Default logger
+    app.logger = create_logger(app)
+    log_level = logging.WARNING
+    if 'LOG_LEVEL' in app.config and app.config['LOG_LEVEL']:
+        log_level = app.config['LOG_LEVEL']
+    app.logger.setLevel(log_level)
+
+    # File logger
+    if 'LOG_FILE' in app.config and app.config['LOG_FILE']:
+        file_handler = RotatingFileHandler(app.config['LOG_FILE'], maxBytes=1024 * 1024 * 100, backupCount=5)
+        file_handler.setLevel(log_level)
+        file_handler.setFormatter(logging.Formatter(
+            '[%(asctime)s] %(levelname)s [%(filename)s %(lineno)d]: %(message)s'
+        ))
+        app.logger.addHandler(file_handler)
+
+    # Email logger
+    if 'EMAIL' in app.config and app.config['EMAIL']:
+        email_log_level = logging.ERROR
+        if 'EMAIL_LOG_LEVEL' in app.config and app.config['EMAIL_LOG_LEVEL']:
+            email_log_level = app.config['EMAIL_LOG_LEVEL']
+        mail_handler = SMTPHandler(
+            mailhost='127.0.0.1',
+            fromaddr=app.config['EMAIL_FROM'] if 'EMAIL_FROM' \
+                in app.config and app.config['EMAIL_FROM'] else "sandhill@sandhill.com",
+            toaddrs=[app.config['EMAIL']],
+            subject=app.config['EMAIL_SUBJECT'] if 'EMAIL_SUBJECT' \
+                in app.config and app.config['EMAIL_SUBJECT'] else 'Sandhill Error'
+        )
+        mail_handler.setLevel(email_log_level)
+        mail_handler.setFormatter(logging.Formatter(
+            '[%(asctime)s] %(levelname)s: %(filename)s %(lineno)d\n%(message)s'
+        ))
+        app.logger.addHandler(mail_handler)
 
 # Ability to load templates from instance/templates/ directory
 loader = ChoiceLoader([
@@ -39,12 +90,8 @@ if app.debug and not "pytest" in sys.modules:
     toolbar = DebugToolbarExtension(app)
     toolbar._success_codes.extend([400, 401, 403, 404, 500, 501])
 
-# Set log level
-app.logger = create_logger(app)
-if app.debug:
-    app.logger.setLevel(logging.DEBUG)
-else:
-    app.logger.setLevel(logging.WARN)
+# Configure logging
+configure_logging()
 
 # Add Sass middleware. This should help us complie CSS from Sass
 if 'COMPILE_SCSS_ON_REQUEST' in app.config and bool(app.config['COMPILE_SCSS_ON_REQUEST']):
