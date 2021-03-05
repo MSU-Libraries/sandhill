314f84ab383f141810ca3e13cbb2073a366fe744
Nathan Collins <colli372@msu.edu>
2021-03-05 16:00:43 -0500

    Fixing solr test calls (clear os env)

--- sandhill/processors/tests/test_solr.py
+++ sandhill/processors/tests/test_solr.py
@@ -8,6 +8,9 @@ from sandhill.utils.test import _test_api_get, _test_api_get_fail, _test_api_get
 from sandhill import app
 
 def test_select():
+    if 'SOLR_URL' in os.environ:
+        del os.environ['SOLR_URL']
+
     data_dict = {
         'params' : {},
         'dummy': 'value'
