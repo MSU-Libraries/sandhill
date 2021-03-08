b6a6615e5f0c67a5a15078865a4764fc4efbe3a7
Megan Schanz <schanzme@msu.edu>
2021-03-08 15:24:37 -0500

    moved test files for scss to crane; renamed TEST_INSTANCE_DIR to INSTANCE_DIR

--- conftest.py
+++ conftest.py
@@ -7,8 +7,8 @@ from flask import Flask
 from importlib import import_module
 cur_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, cur_path)
-if "TEST_INSTANCE_DIR" not in os.environ or not os.environ["TEST_INSTANCE_DIR"]:
-    os.environ["TEST_INSTANCE_DIR"] = os.path.join(cur_path, "sandhill/test_instance/")
+if "INSTANCE_DIR" not in os.environ or not os.environ["INSTANCE_DIR"]:
+    os.environ["INSTANCE_DIR"] = os.path.join(cur_path, "sandhill/test_instance")
 
 def pytest_unconfigure(config):
     mod = import_module('sandhill.utils.generic')
--- sandhill/__init__.py
+++ sandhill/__init__.py
@@ -6,9 +6,10 @@ from flask import Flask
 install_dir = os.path.dirname(os.path.dirname(__file__))
 app = Flask(__name__,
             instance_relative_config=True,
-            instance_path=os.environ["TEST_INSTANCE_DIR"]
-            if "TEST_INSTANCE_DIR" in os.environ
+            instance_path=os.path.join(install_dir, os.environ["INSTANCE_DIR"])
+            if "INSTANCE_DIR" in os.environ
             else None)
+app.install_dir = install_dir
 # Local imports requiring the Flask app
 from sandhill.utils import setup # pylint: disable=wrong-import-position
 from sandhill.routes import main, static, error # pylint: disable=wrong-import-position
