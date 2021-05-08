7d167c991932a931d8094ca6f3e128d189e6432a
Nathan Collins <colli372@msu.edu>
2021-05-07 22:04:30 -0400

    Text coverage for utils.solr

--- sandhill/utils/tests/test_solr_utils.py
+++ sandhill/utils/tests/test_solr_utils.py
@@ -9,3 +9,10 @@ def test_Solr_encode_query():
     assert solr.Solr().encode_query("a value wild-card*") == "a value wild\\-card*"
     assert solr.Solr().encode_query("a value wild-card*", escape_wildcards=True) == "a value wild\\-card\\*"
     assert solr.Solr().encode_query("test query OR (\"sub term\" AND other term)", escape_wildcards=True) == "test query OR (\"sub term\" AND other term)"
+
+def test_Solr_encode_query():
+    testsolr = solr.Solr()
+    testsolr._tokenize_query("test query")
+    _, _ = next(testsolr._get_token())
+    assert testsolr._next_token_is("SPACE")
+    
