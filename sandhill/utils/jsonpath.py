a8c14acbe2d9ef22181efc5ab5713c25e5d41644
Nathan Collins <colli372@msu.edu>
2021-07-19 17:02:58 -0400

    Add jsonpath functions

--- sandhill/utils/jsonpath.py
+++ sandhill/utils/jsonpath.py
@@ -3,6 +3,21 @@ import copy
 from jsonpath_ng import parse
 from jsonpath_ng.jsonpath import Fields, Index
 
+def find(data, path, deepcopy=True):
+    '''
+    Get the values for a given JSONPath
+    args:
+        data (dict|list): The JSON data
+        path (str): The JSONPath to find
+    returns:
+        (list): A list of matches, empty if none found
+    '''
+    if deepcopy:
+        data = copy.deepcopy(data)
+    pattern = parse(path)
+    matches = pattern.find(data)
+    return [match.value for match in matches]
+
 def set(data, path, value, deepcopy=True):
     '''
     Set a value at the given JSONPath location
@@ -18,9 +33,10 @@ def set(data, path, value, deepcopy=True):
     if deepcopy:
         data = copy.deepcopy(data)
     pattern = parse(path)
-    matches = pattern.left.find(data)
     if not isinstance(pattern.right, Fields) and not isinstance(pattern.right, Index):
         raise ValueError("jsonpath.set can only set specific Fields or Indexes")
+
+    matches = pattern.left.find(data)
     for match in matches:
         new_value = match.value
         idxs = [pattern.right.index] if isinstance(pattern.right, Index) \
@@ -29,6 +45,28 @@ def set(data, path, value, deepcopy=True):
             new_value[idx] = value
         parse(str(match.full_path)).update(data, new_value)
 
+    return data
+
+def append(data, path, value, deepcopy=True):
+    '''
+    Append a value to the given JSONPath location. Location must be a list.
+    args:
+        data (dict|list): The JSON data
+        path (str): The JSONPath to a list(s)
+        value (any): The value to append
+    returns:
+        (dict|list): The modified JSON data
+    '''
+    if deepcopy:
+        data = copy.deepcopy(data)
+    pattern = parse(path)
+    matches = pattern.find(data)
+    for match in matches:
+        section = match.value
+        if not isinstance(section, list):
+            raise ValueError(f"jsonpath.append can only do so to a list. Path '{path}' found type {type(section)}")
+        section.append(value)
+        parse(str(match.full_path)).update(data, section)
     return data
 
 def delete(data, path, deepcopy=True):
@@ -45,16 +83,16 @@ def delete(data, path, deepcopy=True):
     if deepcopy:
         data = copy.deepcopy(data)
     pattern = parse(path)
-    matches = pattern.find(data)
+    if not isinstance(pattern.right, Fields) and not isinstance(pattern.right, Index):
+        raise ValueError("jsonpath.set can only set specific Fields or Indexes")
+
+    matches = pattern.left.find(data)
     for match in matches:
-        parent_context = match.context
-        if not isinstance(match.path, Fields) and not isinstance(match.path, Index):
-            raise ValueError("jsonpath.delete can only delete specific Fields or Indexes")
-        new_value = parent_context.value
-        idxs = [match.path.index] if isinstance(match.path, Index) \
-            else match.path.reified_fields(match.context)
+        new_value = match.value
+        idxs = [pattern.right.index] if isinstance(pattern.right, Index) \
+            else pattern.right.reified_fields(match.context)
         for idx in idxs:
             del new_value[idx]
-        parse(str(parent_context.full_path)).update(data, new_value)
+        parse(str(match.full_path)).update(data, new_value)
 
     return data
