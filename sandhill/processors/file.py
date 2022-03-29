7e53ddae264e9c8944fe884dd78974433acf5200
Nathan Collins <colli372@msu.edu>
2022-03-29 03:25:20 -0400

    Mkdocs pilot docs; allow 'path' key for file.load_json

--- sandhill/processors/file.py
+++ sandhill/processors/file.py
@@ -3,7 +3,6 @@ Processing functions for files
 '''
 import os
 import io
-import collections
 from operator import itemgetter
 from flask import json
 from requests.models import Response as RequestsResponse
@@ -14,52 +13,60 @@ from sandhill.utils.template import evaluate_conditions
 
 def load_json(data):
     '''
-    Looks for paths within the 'paths' key of the data and will
-    search for those files within the instance directory. It will stop
-    when it finds a file and will return the contents as json.
-    args:
-        data(dict): all of the previously loaded data, typically from a route
-    returns:
-        json: The first file found in the instance_path with the given path
+    Search for files at the paths within 'path' and 'paths' keys of `data`.
+    Will load JSON from the first file it finds and then return the result.
+    If both 'path' and 'paths' are set, paths from both will be searched
+    starting with 'path' first.\n
+    Args:
+        data (dict): Processor arguments and all other data loaded from previous data processors.\n
+         * `path` _string_: A single file path to search for.\n
+         * `paths` _list_: A list of file paths to search for.\n
+    Note:
+        Paths must be relative to the `instance/` directory.
+    Returns:
+        The loaded JSON data or None if no file was found.
     '''
-    file_data = collections.OrderedDict()
+    file_data = None
     # loop over each provided path and stop when one is found
-    if 'paths' in data:
-        for path in data['paths']:
-            full_path = os.path.join(app.instance_path, path)
+    if "path" in data:
+        data.setdefault("paths", []).insert(0, data["path"])
+    if "paths" in data:
+        for path in data["paths"]:
+            full_path = os.path.join(app.instance_path, path.lstrip("/"))
             if os.path.exists(full_path):
                 file_data = load_json_config(full_path)
                 break
-
     return file_data
 
 def create_json_response(data):
     '''
-    Wrapper for load_json that will return a json response object.
-    This can be used if you wish to stream json instead of use it's data
-    for a template.
-    args:
-        data(dict): all of the previously loaded data, typically from a route
-    returns:
-        Response: response object with the json data loaded
+    Wrapper for `load_json` that will return a JSON response object.
+    This can be used to stream JSON instead of loading it to use it as data.
+    Args:
+        data (dict): Processor arguments and all other data loaded from previous data processors.\n
+         * `path` _string_: A single file path to search for.\n
+         * `paths` _list_: A list of file paths to search for.\n
+    Returns:
+        A `requests.Response` instance with the JSON data loaded into it.
     '''
     resp = RequestsResponse()
     resp.status_code = 200
 
     content = load_json(data)
-
     if content:
         resp.raw = io.StringIO(json.dumps(content))
-
     return resp
 
 def load_matched_json(data):
     """
-    Loads all the config files and returns the file that has the maximum matched conditions
-    args:
-        data(dict): context data as previously loaded, typically from a route
-    returns:
-        json: data from the matched route config based on the most match_conditions
+    Loads all the config files and returns the file that has the most [matched conditions](#TODO).
+    Args:
+        data (dict): Processor arguments and all other data loaded from previous data processors.\n
+         * `location` _string_: A directory path within the instance \
+           with JSON files containing `match_conditions` keys.\n
+    Returns:
+        The loaded JSON data from the file that most matched its conditions, or None if \
+        no files matched.
     """
     file_data = None
     matched_dict = {}
@@ -79,11 +86,11 @@ def load_matched_json(data):
     matched_path = max(matched_dict.items(), key=itemgetter(1))[0] if matched_dict else None
 
     for path, score in matched_dict.items():
-        app.logger.debug(f"load_matched_json - score: {score}, path: {path}")
+        app.logger.debug(f"load_matched_json(score={score}, path={path})")
 
     # Ensure number of matches is greater than 0
     if matched_path in matched_dict and matched_dict[matched_path]:
-        app.logger.info(f"load_matched_json - matched: {matched_path}")
+        app.logger.debug(f"load_matched_json(matched={matched_path})")
         file_data = config_files[matched_path]
 
     return file_data
