0fcde083716e644c260b0b7597e2741e3f13eeb7
Nathan Collins <colli372@msu.edu>
2023-03-23 02:51:29 -0400

    Update deps, rem sub-deps in requirements.txt; fix static route

--- sandhill/routes/static.py
+++ sandhill/routes/static.py
@@ -5,8 +5,8 @@ import os
 from flask import send_from_directory
 from sandhill import app
 
-@app.route('/static/<path:static_file>')
-def handle_static(static_file):
+@app.route('/static/<path:filename>', endpoint='static')
+def handle_static(filename):
     '''
     Replacement for the default Flask `/static` path handler.
     Retrieves the requested static file by first looking for it inside
@@ -14,7 +14,7 @@ def handle_static(static_file):
     method will then look for the file in the core `sandhill/templates/`
     directory.
     Args:
-        static_file (str): The requested file path within `/static`
+        filename (str): The requested file path within `/static`
     Returns:
         (file stream): File stream of the file object
     Raises:
@@ -24,11 +24,11 @@ def handle_static(static_file):
     static_path = os.path.join(app.instance_path, "static")
 
     # Fall back to sandhill/static/
-    if not os.path.isfile(os.path.join(static_path, static_file)):
+    if not os.path.isfile(os.path.join(static_path, filename)):
         static_path = os.path.join(app.root_path, "static")
 
-    cache_timeout = app.get_send_file_max_age(static_file)
-    return send_from_directory(static_path, static_file, cache_timeout=cache_timeout)
+    cache_timeout = app.get_send_file_max_age(filename)
+    return send_from_directory(static_path, filename, cache_timeout=cache_timeout)
 
 @app.route('/favicon.ico')
 def favicon():
