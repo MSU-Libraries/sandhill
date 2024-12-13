5b51e1ac1e06bacb2a4eeb38dda3fc07278d36e3
Nathan Collins <colli372@msu.edu>
2024-12-13 03:21:38 -0500

    sandcache better docstring and unittest

--- sandhill/utils/sandcache.py
+++ sandhill/utils/sandcache.py
@@ -10,7 +10,13 @@ from sandhill import app
 @contextmanager
 def sandcache():
     """
-    Opens the default diskcache cache for Sandhill
+    Opens the default diskcache cache for Sandhill. Operates like a dict, but
+    writes to a database within /tmp/crane-cache/
+    ```
+    with sandcache() as cache:
+        cache['data'] = data_to_cache
+        quick_load = cache['data_cached']
+    ```
     """
     cachedir = os.path.join(tempfile.gettempdir(), 'crane-cache')
     cache = Cache(cachedir, size_limit=app.config.get('DISKCACHE_SIZE_GB', 1) * 1024**3)
